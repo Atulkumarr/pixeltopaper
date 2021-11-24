0b6b66bc0ae244e4ba25716613087b5e753168fd
Atul Kumar <atul170301@gmail.com>
2021-11-24 18:04:55 +0530

    check

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplaySpeed: 1500,
+    autoplayHoverPause: false,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
