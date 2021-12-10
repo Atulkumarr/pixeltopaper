b4dda1ac0aba393ec57517f41c9401a839b59563
Atul Kumar <atul170301@gmail.com>
2021-12-10 15:50:45 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
-$('.banner-slider').owlCarousel({
-    loop: true,
-    margin: 0,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplaySpeed: 1500,
-    autoplayHoverPause: false,
-    nav: false,
-    dots: false, 
-    mouseDrag: false,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut',
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
+// for modal
+
+$(".product-box,.hover-div,.last-img").click(function () {
+  $(".click-box").addClass("add-me");
 });
-$('.wall-mount-slider').owlCarousel({
-    margin: 20,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplaySpeed: 1500,
-    autoplayHoverPause: false,
-    nav: true,
-    dots: false,
-    // loop: false,
-    loop: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        }
-    }
+$(".close-click").click(function () {
+  $(".click-box").removeClass("add-me");
+});
+
+// owl
+$(".banner-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  autoplaySpeed: 1500,
+  autoplayHoverPause: false,
+  nav: false,
+  dots: false,
+  mouseDrag: false,
+  animateIn: "fadeIn",
+  animateOut: "fadeOut",
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+$(".wall-mount-slider").owlCarousel({
+  margin: 20,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  autoplaySpeed: 1500,
+  autoplayHoverPause: false,
+  nav: true,
+  dots: false,
+  // loop: false,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
 });
